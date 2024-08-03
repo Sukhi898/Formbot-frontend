eb243aeb55227ed5888e8e52a8806cb76a236cd2
Sukhi898 <146666624+Sukhi898@users.noreply.github.com>
2024-08-04 01:15:39 +0530

    Update Forms.js

--- src/apis/Forms.js
+++ src/apis/Forms.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BACKEND_URL = "https://ec2-13-60-104-236.eu-north-1.compute.amazonaws.com";
+const BACKEND_URL = "https://formbot-api-nwz1.onrender.com";
 
 const SaveFormdetails = async ({ formName, formData, userid }) => {
   try {
